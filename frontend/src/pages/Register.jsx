f872dfb7bceb3d52c70cdd05581aba54863c3539
Decayy25 <mochrizqihermawan615@gmail.com>
2026-02-21 05:42:08 +0700

    fix: clean code

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
