355d50ba298b402aaedbb44d50196c65d5ab55db
Decayy25 <mochrizqihermawan615@gmail.com>
2026-02-20 22:08:17 +0700

    fix: API in Login and Register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5050/api/auth/register", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
