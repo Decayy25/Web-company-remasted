6ad11793af752070b22267a979fb7bd92ccf43ea
Decayy25 <mochrizqihermawan615@gmail.com>
2026-04-17 16:54:29 +0700

    refac: navigation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { useEffect }  from "react";
 import feather from "feather-icons"
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../assets/img/profile.png";
 
 const Istagram = "https://www.instagram.com/smart_clk/";
@@ -39,24 +39,24 @@ export default function Footer() {
           </h3>
           <ul className="space-y-2 text-sm text-gray-300">
             <li>
-              <Navigate to="/BantuanAkademik" className="hover:text-[#51a2ff]">
+              <Link to="/BantuanAkademik" className="hover:text-[#51a2ff]">
                 Bantuan Akademik
-              </Navigate>
+              </Link>
             </li>
             <li>
-              <Navigate to="/Kurikulum" className="hover:text-[#51a2ff]">
+              <Link to="/Kurikulum" className="hover:text-[#51a2ff]">
                 Informasi Kurikulum
-              </Navigate>
+              </Link>
             </li>
             <li>
-              <Navigate to="/TataTertib" className="hover:text-[#51a2ff]">
+              <Link to="/TataTertib" className="hover:text-[#51a2ff]">
                 Tata Tertib Sekolah
-              </Navigate>
+              </Link>
             </li>
             <li>
-              <Navigate to="/ProfileGuru" className="hover:text-[#51a2ff]">
+              <Link to="/ProfileGuru" className="hover:text-[#51a2ff]">
                 Profil Guru
-              </Navigate>
+              </Link>
             </li>
           </ul>
         </div>
